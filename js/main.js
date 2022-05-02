d8f44cf18c147522ae390bb8c4d575c532d884a7
Niko1248 <Sirius04@bk.ru>
2022-05-02 21:12:41 +0300

    Изменил кнопку,внес коррективы

--- js/main.js
+++ js/main.js
@@ -106,7 +106,7 @@ window.onload = function () {
 	menuCloseItem.addEventListener('click', () => {
 		menu.classList.remove('header__nav_active');
 	});
-	if (window.innerWidth <= 750) {
+	if (window.innerWidth >= 1) {
 		for (let i = 0; i < menuLinks.length; i += 1) {
 			menuLinks[i].addEventListener('click', () => {
 				menu.classList.remove('header__nav_active');
